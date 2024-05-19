bfd95cb916f38241a78c52bd4a5fc3e54e904fdd
Vim-al <iamvimal15@gmail.com>
2024-05-19 19:59:44 +0530

    create read update without database

--- server.js
+++ server.js
@@ -1,1 +1,83 @@
-console.log("servers is working....");
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
+import { nanoid } from "nanoid";
+
+let jobs = [
+  { id: nanoid(), company: "apple", position: "front-end" },
+  { id: nanoid(), company: "google", position: "back-end" },
+];
+
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.send({ message: "data recieved", data: req.body });
+});
+
+//GET ALL JOBS
+
+app.get("/api/v1/jobs", (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+//CREATE JOB
+
+app.post("/api/v1/jobs", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "please provide company and position" });
+  }
+  const id = nanoid(10);
+  // console.log(id);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(201).json({ job });
+});
+
+//GET JOB
+
+app.get("/api/v1/jobs/:id", (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+  res.status(200).json({ job });
+});
+
+//UPDATE JOB
+
+app.patch("/api/v1/jobs/:id", (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(400).json({ msg: "provide company and position" });
+  }
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `job not found with id: ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+
+  res.status(200).json({ msg: "job modified", job });
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
