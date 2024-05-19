609b9a1cd2aa8244373aca7e6962fdf5a4e429aa
Vim-al <iamvimal15@gmail.com>
2024-05-20 00:04:26 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import router from "./routes/jobRouter.js";
+import mongoose from "mongoose";
 
 if (process.env.NODE_ENV == "development") {
   app.use(morgan("dev"));
@@ -32,7 +33,12 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
