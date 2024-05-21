9e6fa7c07935e87e460c3db1d4aa94adb9a70c66
Vim-al <iamvimal15@gmail.com>
2024-05-21 14:42:27 +0530

    validated register input and modified role

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,11 @@
+import { StatusCodes } from "http-status-codes";
+import User from "../models/userModel.js";
+
 export const register = async (req, res) => {
-  res.send("register");
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 export const login = async (req, res) => {
