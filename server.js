930a2a40b2c945216cf4a53f94117dd9c2d767a2
Vim-al <iamvimal15@gmail.com>
2024-05-21 22:46:16 +0530

    updated job model, set up authMiddleware, verify Jwt token

--- server.js
+++ server.js
@@ -8,11 +8,14 @@ import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
 import mongoose from "mongoose";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
+import cookieParser from "cookie-parser";
 
 if (process.env.NODE_ENV == "development") {
   app.use(morgan("dev"));
 }
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -24,7 +27,7 @@ app.post("/api/v1/test", (req, res) => {
   res.send({ message: "data received", data: req.body });
 });
 
-app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
