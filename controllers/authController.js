d787054cdc7110f0d19b26d1916e0bfcaf2ea502
Vim-al <iamvimal15@gmail.com>
2024-05-21 16:53:27 +0530

    Http cookie sent

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,5 +21,13 @@ export const login = async (req, res) => {
   if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
 
   const token = createJWT({ userId: user._id, role: user.role });
-  res.json({ token });
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  res.status(StatusCodes.CREATED).json({ msg: "user logged in" });
 };
