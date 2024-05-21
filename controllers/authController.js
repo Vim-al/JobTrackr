4d90a140818e10d5596c46b1e29df00d35dc7a47
Vim-al <iamvimal15@gmail.com>
2024-05-21 16:11:26 +0530

    validated login input

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -13,5 +14,9 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
   res.send("login");
 };
