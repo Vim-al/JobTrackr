eeea8131d6ffebc9e1ea4b79c77e1b7835ba16b2
Vim-al <iamvimal15@gmail.com>
2024-05-21 12:55:48 +0530

    Validation of job input and job id done

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -15,26 +15,17 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-  if (!job) {
-    throw new NotFoundError(`no job with id ${id}`);
-  }
   res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
   const { id } = req.params;
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, { new: true });
-  if (!updatedJob) {
-    throw new NotFoundError(`no job with id ${id}`);
-  }
   res.status(StatusCodes.OK).json({ msg: "job modified", job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const deletedJob = await Job.findByIdAndDelete(id);
-  if (!deletedJob) {
-    throw new NotFoundError(`no job with id ${id}`);
-  }
   res.status(StatusCodes.OK).json({ msg: "job deleted", job: deletedJob });
 };
