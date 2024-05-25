5331827e04254266afcfc07450714cb66cff224c
Vim-al <iamvimal15@gmail.com>
2024-05-25 17:28:30 +0530

    create, delete, edit, all jobs functionalities and pages completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,10 +11,16 @@ import {
   AllJobs,
   Admin,
   Profile,
+  EditJob,
 } from "./pages";
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
+import { action as addJobAction } from "./pages/AddJob";
+import { loader as allJobsLoader } from "./pages/AllJobs";
+import { loader as editJobLoader } from "./pages/EditJob";
+import { action as editJobAction } from "./pages/EditJob";
+import { action as deleteJobAction } from "./pages/DeleteJob";
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -52,6 +58,7 @@ const router = createBrowserRouter([
           {
             index: true,
             element: <AddJob />,
+            action: addJobAction,
           },
           {
             path: "stats",
@@ -60,6 +67,7 @@ const router = createBrowserRouter([
           {
             path: "all-jobs",
             element: <AllJobs />,
+            loader: allJobsLoader,
           },
           {
             path: "admin",
@@ -68,6 +76,16 @@ const router = createBrowserRouter([
           {
             path: "profile",
             element: <Profile />,
+          },
+          {
+            path: "edit-job/:id",
+            element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
+          },
+          {
+            path: "delete-job/:id",
+            action: deleteJobAction,
           },
         ],
       },
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,8 @@ export { default as FormRow } from "./FormRow";
 export { default as SmallSidebar } from "./SmallSidebar";
 export { default as BigSidebar } from "./BigSidebar";
 export { default as Navbar } from "./Navbar";
+export { default as FormRowSelect } from "./FormRowSelect";
+export { default as SearchContainer } from "./SearchContainer";
+export { default as JobsContainer } from "./JobsContainer";
+export { default as Job } from "./Job";
+export { default as JobInfo } from "./JobInfo";
