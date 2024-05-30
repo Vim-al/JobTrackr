2ed09e07f9b7471519c0801635afd3766eaa99d0
vastcsbs <vastcsbs@gmail.com>
2024-05-30 09:45:04 +0530

    Update user functionality with cloudinary

--- server.js
+++ server.js
@@ -11,13 +11,27 @@ import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/userRouter.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+import cloudinary from "cloudinary";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 if (process.env.NODE_ENV == "development") {
   app.use(morgan("dev"));
 }
 
+app.use(express.static(path.resolve(__dirname, "./public")));
+
 app.use(cookieParser());
 app.use(express.json());
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 app.get("/api/v1/test", (req, res) => {
   res.send("Hello world");
