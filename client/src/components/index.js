8ef1fa7ae06c58a294d866d2a9b6eebfe4d84c43
Vim-al <iamvimal15@gmail.com>
2024-06-02 16:13:28 +0530

    Stats page completed

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -9,3 +9,5 @@ export { default as JobsContainer } from "./JobsContainer";
 export { default as Job } from "./Job";
 export { default as JobInfo } from "./JobInfo";
 export { default as StatItem } from "./StatItem";
+export { default as StatsContainer } from "./StatsContainer";
+export { default as ChartsContainer } from "./ChartsContainer";
